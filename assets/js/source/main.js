872347e87e649612198a201061f684c09f65e883
Mark Bain <hello@markbaindesign.com>
2014-04-24 12:55:46 +0200

    Added "back to top" scroll

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -1,5 +1,25 @@
 (function($) {
 
-	// all Javascript code goes here
+/*
+=====================================================
+		
+		Back to top scrolling
+	
+=====================================================
+*/		
+
+jQuery(document).ready(function($) { // Wrap all scripts in this
+
+	$(document).ready(function(){
+		// scroll body to 0px on click
+		$('#back-to-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+});
 
 })(jQuery);
