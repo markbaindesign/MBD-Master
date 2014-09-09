a9b3f0ccf5a4f5595c8bc3fd4774a69f4abca153
Mark Bain <hello@markbaindesign.com>
2014-09-09 19:06:20 +0200

    updated

--- single.php
+++ single.php
@@ -9,7 +9,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+<div class="section">
+<div class="container">
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
@@ -21,8 +22,14 @@ get_header(); ?>
 				if ( comments_open() || '0' != get_comments_number() ) :
 					comments_template();
 				endif;
-			?>
-
+?>
+</div>
+</div>
+<div class="section post-navigation">
+<div class="container">
+			<?php mbdmaster_post_nav(); ?>
+</div>
+</div>
 		<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
