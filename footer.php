c625da0851b0759b89816839c065005042562c96
Mark Bain <hello@markbaindesign.com>
2014-09-09 19:05:52 +0200

    updated

--- footer.php
+++ footer.php
@@ -1,20 +1,11 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package _mbbasetheme
- */
-?>
 
 	</div><!-- #content -->
 	
 	<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-		<div id="footer-widgets" class="widget-area">
+		<div id="footer-widgets" class="widget-area section">
 			
 				
-				<div class="masonrycontainer">
+				<div class="masonrycontainer container">
 					<div class="grid-sizer"></div>
 					<div class="gutter-sizer"></div>				
 					<?php dynamic_sidebar( 'sidebar-2' ); ?>
@@ -25,22 +16,24 @@
 			
 		</div><!-- .widget-container -->
 	<?php endif; ?>
-	<footer id="colophon" class="site-footer" role="contentinfo">
-	
+	<footer id="colophon" class="section site-footer" role="contentinfo">
+		<div class="container">
 		<div class="site-info">
-			<p id="copyright">
-				<a href="<?php echo bloginfo( 'url' ); ?>">&copy; <?php echo date("Y"); ?> <?php echo bloginfo( 'name' ); ?></a>
-			</p> 
-			<p id="tagline">
+			<div id="copyright">
+					<p><a href="<?php echo bloginfo( 'url' ); ?>">&copy; <?php echo date("Y"); ?> <?php echo bloginfo( 'name' ); ?></a></p>
+			</div> 
+		<!--	<p id="tagline">
 				<?php echo get_bloginfo( 'description' ); ?>
-			</p> 
+			</p> --> 
 		</div><!-- .site-info -->
 
-		<ul id="social" class="clearfix footer-block">
-			<li><a href="https://www.facebook.com"><i aria-hidden="true" class="icon-facebook icon-left"></i><span class="">Facebook</span></a></li> 
-			<li><a href="https://twitter.com"><i aria-hidden="true" class="icon-twitter icon-left"></i><span class="">Twitter</span></a></li> 
-			<li><a href="<?php bloginfo('rss2_url'); ?>"><i aria-hidden="true" class="icon-feed icon-left"></i><span class="">RSS</span></a></li> 
-		</ul><!-- #social -->
+		<div class="social">
+			<ul class="social-media-links">
+				<li><a href="https://www.facebook.com/markbaindesign"><i aria-hidden="true" class="icon-facebook icon-left"></i><span class="">Facebook</span></a></li> 
+				<li><a href="https://twitter.com/mbain"><i aria-hidden="true" class="icon-twitter icon-left"></i><span class="">Twitter</span></a></li> 
+				<li><a href="<?php bloginfo('rss2_url'); ?>"><i aria-hidden="true" class="icon-feed icon-left"></i><span class="">RSS</span></a></li> 
+			</ul>
+		</div><!-- .social -->
 
 		<div id="design">
 			<p>Designed <span class="amp">&amp;</span> coded by <a href="http://markbaindesign.com" title="Visit the website of Mark Bain Design">Mark Bain Design</a></p>
@@ -49,7 +42,7 @@
 		<div id="back-to-top" >
 			<a href="#header" title="Go back to the top" class=""><span class="si-icon si-icon-nav-up-arrow" data-icon-name="navUpArrow">Back to top</span></a>
 		</div><!-- #back-to-top -->
-
+		</div><!-- .container -->
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
