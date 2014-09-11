526efc708071e10d2e94b83191752e61d32bdd6c
Mark Bain <hello@markbaindesign.com>
2014-09-11 17:26:54 +0200

    quotes module

---
+++ content-quotes.php
@@ -0,0 +1,10 @@
+<h3>What people are saying about...</h3>
+<h5>You had me at Hello</h5>
+<blockquote>
+	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed eleifend urna eu sapien. Quisque posuere nunc eu massa. Praesent bibendum lorem non leo. Morbi volutpat, urna eu fermentum rutrum, ligula lacus interdum mauris, ac pulvinar libero pede a enim. Etiam commodo malesuada ante. Donec nec ligula. Curabitur mollis semper diam.</p>
+</blockquote>
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/avatar.png" alt="Steve Jobs">
+<span class="meta">&mdash;<cite>Steve Jobs</cite></span>
+<div class="readmore">
+	<a href="" title="See more of these">See more</a>
+</div>
