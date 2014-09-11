ec3c8b16f174f50ead07d2ff5fbd9032dea9ebfb
Mark Bain <hello@markbaindesign.com>
2014-09-11 15:33:48 +0200

    more masonry

---
+++ content-signup-form.php
@@ -0,0 +1,16 @@
+<form action="http://eltjam.us8.list-manage1.com/subscribe/post?u=f5da7118912d25ef243e5753d&amp;id=a710113228" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate><div style="position: absolute; display: table-cell; left: -5000px;"><input type="text" name="b_f5da7118912d25ef243e5753d_a710113228" tabindex="-1" value=""></div>
+<p class="username"> 
+							  <label for="usernamesignup" class="uname" data-icon="u">Your name</label>
+							  <input id="usernamesignup" name="FNAME" required="required" type="text" placeholder="i.e. John Doe" />
+						 </p>					 <p class="emailaddress"> 
+							  <label for="emailsignup" class="youmail" data-icon="e" > Your email</label>
+							  <input id="emailsignup" name="EMAIL" required="required" type="email" placeholder="i.e. johndoe@mail.com"/> 
+						</p>
+<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    
+
+						 <p class="signin button"> 
+						 <input type="submit" class="cta button cta-primary" value="Sign up"/><!-- <span> or find out more &rarr;</span> -->						
+						 </p>
+
+					</form>
