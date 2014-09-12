ba887eb347b6dbca43a5f1b44cef83f832f72e64
Mark Bain <hello@markbaindesign.com>
2014-09-12 19:04:47 +0200

    Set up flexsider js

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -94,3 +94,74 @@ jQuery(document).ready(function($) { // Wrap all scripts in this
 	
 */
 }); // end Wrap all scripts in this
+
+/*
+=====================================================
+		
+		Google Fonts 
+	
+=====================================================
+*/	
+
+jQuery(document).ready(function($) { // Wrap all scripts in this
+
+WebFontConfig = {
+    google: { families: [ 'Noto+Sans:400,700,400italic,700italic:latin', 'Noto+Serif:400,700,400italic,700italic:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+}); // Wrap all scripts in this
+
+/*
+=====================================================
+		
+		WordPress Content Slider 
+	
+=====================================================
+*/	
+
+jQuery(document).ready(function($) { // Wrap all scripts in this
+
+	var body    = $( 'body' ),
+	    _window = $( window );
+
+	// Initialize Featured Content slider.
+	_window.load( function() {
+		if ( body.is( '.slider' ) ) {
+			$( '.featured-content' ).featuredslider( {
+				selector:  '.featured-content-inner > article',
+				controlsContainer: '.featured-content'
+			} );
+		}
+	} );
+
+}); // end Wrap all scripts in this
+
+/*
+=====================================================
+		
+		WordPress Content Slider 
+	
+=====================================================
+*/	
+
+jQuery(document).ready(function($) { // Wrap all scripts in this
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+animationLoop: false,
+    slideshow: false,
+			animation: "slide",
+    		controlNav: "thumbnails",
+		 directionNav: false;
+	 	});
+  });
+
+}); // end Wrap all scripts in this
