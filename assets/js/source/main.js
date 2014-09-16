8f4684a9d99764490cec34c70e41191249c01236
Mark Bain <hello@markbaindesign.com>
2014-09-16 19:40:58 +0200

    hidden search added to header

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -39,24 +39,16 @@ jQuery( document ).ready( function( $ ) {
 */	
 
 jQuery(document).ready(function( $ ){
-	var navigation = responsiveNav(".nav-collapse", {
-			animate: true,                    // Boolean: Use CSS3 transitions, true or false
-			transition: 284,                  // Integer: Speed of the transition, in milliseconds
-			label: "Menu",                    // String: Label for the navigation toggle
-			insert: "before",                  // String: Insert the toggle before or after the navigation
+
+ var navigation2 = responsiveNav(".hidden-search", {
+     customToggle: "search-toggle",
+ });
+
+	var navigation1 = responsiveNav(".main-navigation", {
 			customToggle: "nav-toggle",                 // Selector: Specify the ID of a custom toggle
-			closeOnNavClick: false,           // Boolean: Close the navigation when one of the links are clicked
-			openPos: "relative",              // String: Position of the opened nav, relative or static
-			navClass: "nav-collapse",         // String: Default CSS class. If changed, you need to edit the CSS too!
-			navActiveClass: "js-nav-active",  // String: Class that is added to <html> element when nav is active
-			jsClass: " ",                    // String: 'JS enabled' class which is added to <html> element
 			// Swapping no-js to js with script instead
-			init: function(){},               // Function: Init callback
-			open: function(){},               // Function: Open callback
-			close: function(){}               // Function: Close callback
 		});
 	});
-
 
 
 /*
@@ -160,7 +152,7 @@ animationLoop: false,
     slideshow: false,
 			animation: "slide",
     		controlNav: "thumbnails",
-		 directionNav: false;
+		 directionNav: false,
 	 	});
   });
 
