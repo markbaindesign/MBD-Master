732a8254d8105b01bd1dbd2fdec9a9ee1d03a903
Mark Bain <hello@markbaindesign.com>
2014-09-16 19:41:26 +0200

    hidden search changes to markup

--- footer.php
+++ footer.php
@@ -33,30 +33,30 @@
 
 	<footer id="colophon" class="section site-footer" role="contentinfo">
 		<div class="container">
-		<div class="site-info">
-			<div id="copyright">
-					<p><a href="<?php echo bloginfo( 'url' ); ?>">&copy; <?php echo date("Y"); ?> <?php echo bloginfo( 'name' ); ?></a></p>
-			</div> 
-		<!--	<p id="tagline">
-				<?php echo get_bloginfo( 'description' ); ?>
-			</p> --> 
-		</div><!-- .site-info -->
+
 
 		<div class="social">
 			<ul class="social-media-links">
-				<li><a href="https://www.facebook.com/markbaindesign"><i aria-hidden="true" class="icon-facebook icon-left"></i><span class="">Facebook</span></a></li> 
-				<li><a href="https://twitter.com/mbain"><i aria-hidden="true" class="icon-twitter icon-left"></i><span class="">Twitter</span></a></li> 
-				<li><a href="<?php bloginfo('rss2_url'); ?>"><i aria-hidden="true" class="icon-feed icon-left"></i><span class="">RSS</span></a></li> 
+				<li><a href="https://twitter.com/mbain"><i aria-hidden="true" class="icon-twitter icon-left"></i><span class="visuallyhidden">Twitter</span></a></li> 
+				<li><a href="https://www.facebook.com/markbaindesign"><i aria-hidden="true" class="icon-facebook icon-left"></i><span class="visuallyhidden">Facebook</span></a></li> 
+				<!-- <li><a href="<?php bloginfo('rss2_url'); ?>"><i aria-hidden="true" class="icon-feed icon-left"></i><span class="visuallyhidden">RSS</span></a></li>-->
 			</ul>
 		</div><!-- .social -->
-
-		<div id="design">
-			<p>Designed <span class="amp">&amp;</span> coded by <a href="http://markbaindesign.com" title="Visit the website of Mark Bain Design">Mark Bain Design</a></p>
-		</div><!-- #design -->
 
 		<div id="back-to-top" >
 			<a href="#header" title="Go back to the top" class=""><span class="si-icon si-icon-nav-up-arrow" data-icon-name="navUpArrow">Back to top</span></a>
 		</div><!-- #back-to-top -->
+
+
+		<div class="site-info">
+			<div id="copyright">&copy; <?php echo date("Y"); ?> <?php echo bloginfo( 'name' ); ?></div> 
+		<!--	<p id="tagline">
+				<?php echo get_bloginfo( 'description' ); ?>
+			</p> --> 
+		<div id="design"><!-- Designed <span class="amp">&amp;</span> coded -->Made by <a href="http://markbaindesign.com" title="Visit the website of Mark Bain Design">Mark Bain Design</a>
+		</div><!-- #design -->
+
+		</div><!-- .site-info -->
 		</div><!-- .container -->
 	</footer><!-- #colophon -->
 
