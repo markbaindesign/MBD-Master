ab5aa69185d1e4d6b5f5122380da50f2dd441def
Mark Bain <hello@markbaindesign.com>
2014-09-19 09:35:38 +0200

    add purchase options module

---
+++ content-purchase-options.php
@@ -0,0 +1,10 @@
+<h2>Purchase options</h2>
+<p>Aliquam mattis nulla ac augue ornare, quis ullamcorper nibh pharetra.</p>
+<ul>
+<li><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+<li><a href="">Nunc quis lacus commodo, mollis orci in, accumsan sapien.</a></li>
+<li><a href="">Vestibulum convallis felis ac lorem maximus ullamcorper.</a></li>
+<li><a href="">Morbi aliquet justo eget sodales elementum.</a></li>
+<li><a href="">Phasellus cursus orci et hendrerit imperdiet.</a></li>
+<li><a href="">Nunc vestibulum purus vel tristique ultrices.</a></li>
+</ul>
