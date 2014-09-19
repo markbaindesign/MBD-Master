46dddfe397d27ab3e46908d2100123257504728c
Mark Bain <hello@markbaindesign.com>
2014-09-19 09:39:36 +0200

    remove comments from pages

--- page.php
+++ page.php
@@ -1,16 +1,4 @@
-<?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package _mbbasetheme
- */
-
-get_header(); ?>
+<?php /* Page */ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -21,12 +9,7 @@ get_header(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+
 
 			<?php endwhile; // end of the loop. ?>
 </div>
